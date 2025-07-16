a53bd90a9334ee2ae3436560480f339b2d03e0c7
Ryan Chen <ryan_chen@berkeley.edu>
2025-07-15 22:13:18 -0700

    graphics testing

--- client/scenes/MainMenu.js
+++ client/scenes/MainMenu.js
@@ -1,32 +1,58 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class MainMenu extends Scene
-{
-    constructor ()
-    {
-        super('MainMenu');
-    }
+export class MainMenu extends Scene {
+  constructor() {
+    super("MainMenu");
+    this.selectedVariation = null;
+  }
 
-    create ()
-    {
-        const bg = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background');
-        let scaleX = this.cameras.main.width / bg.width + 0.2;
-        let scaleY = this.cameras.main.height / bg.height + 0.2;
-        let scale = Math.max(scaleX, scaleY);
-        bg.setScale(scale).setScrollFactor(0);
+  create() {
+    const bg = this.add.image(
+      this.cameras.main.width / 2,
+      this.cameras.main.height / 2,
+      "background"
+    );
+    let scaleX = this.cameras.main.width / bg.width + 0.2;
+    let scaleY = this.cameras.main.height / bg.height + 0.2;
+    let scale = Math.max(scaleX, scaleY);
+    bg.setScale(scale).setScrollFactor(0);
 
-        this.add.image(this.game.config.width * 0.5, 300, 'logo');
+    this.add.image(this.game.config.width * 0.5, 300, "logo");
 
-        this.add.text(this.game.config.width * 0.5, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+    this.add
+      .text(this.game.config.width * 0.5, 460, "Main Menu", {
+        fontFamily: "Arial Black",
+        fontSize: 38,
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 8,
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
+    // Variation Button
+    // Draw rounded rectangle using Graphics
+    this.add.image(this.game.config.width * 0.5, 540, "button");
 
-            this.scene.start('Game');
+    const variationButton = this.add
+      .text(this.game.config.width / 2, 540, "Variation 1", {
+        fontSize: "28px",
+        color: "#000000",
+        //backgroundColor: "#333333",
+        padding: { x: 20, y: 10 },
+        border: "2px solid #000000",
+      })
+      .setOrigin(0.5)
+      .setInteractive({ useHandCursor: true });
 
-        });
-    }
+    variationButton.on("pointerdown", () => {
+      this.selectedVariation = "variation_1";
+      console.log(this.selectedVariation);
+      //variationButton.setStyle({ backgroundColor: "#5555ff" }); // visual feedback
+    });
+
+    // this.input.once("pointerdown", () => {
+    //   this.scene.start("Game");
+    // });
+  }
 }
--- client/scenes/Preloader.js
+++ client/scenes/Preloader.js
@@ -27,6 +27,10 @@ export class Preloader extends Scene {
         });
     }
 
+    preload() {
+        this.load.image('button', 'assets/button.png')
+    }
+
     create() {
         //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
         //  For example, you can define global animations here, so we can use them in other scenes.
